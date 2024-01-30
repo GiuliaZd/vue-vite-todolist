3f1fd7e9f41dc05ed8a7688d8919eb84b068568b
GiuliaZd <yuliya.zd@gmail.com>
2024-01-30 21:23:43 +0200

    added keys , v-for and todo items list in array of objects

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <h1>Todo List</h1>
   <ul>
-    <li>
-      <to-do-item label="Todo task" :done="true"></to-do-item>
+    <li v-for="item in ToDoItems" :key="item.id">
+      <to-do-item :label="item.label" :done="item.done"></to-do-item>
     </li>
   </ul>
 </template>
 
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
 
 export default {
   name: "App",
   components: { ToDoItem },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "Study JS", done: true },
+        { id: uniqueId("todo-"), label: "Study TS", done: false },
+        { id: uniqueId("todo-"), label: "Prepare for exams", done: true },
+        { id: uniqueId("todo-"), label: "Write the thesis", done: false },
+        { id: uniqueId("todo-"), label: "Learn C++", done: false },
+      ],
+    };
+  },
 };
 </script>
 
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <input type="checkbox" id="todo-item" :checked="isDone" />
-    <label for="todo-item">{{ label }}</label>
+    <input type="checkbox" id="id" :checked="isDone" />
+    <label for="id">{{ label }}</label>
   </div>
 </template>
 
 <script>
+import uniqueId from "lodash.uniqueid";
 export default {
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
   },
   data() {
-    return { isDone: this.done };
+    return { isDone: this.done, id: uniqueId("todo-") };
   },
 };
 </script>

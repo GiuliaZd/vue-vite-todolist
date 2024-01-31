89a5ea9ff31d1e0eac10da66002b3c73452accf3
GiuliaZd <yuliya.zd@gmail.com>
2024-01-31 13:13:27 +0200

    added input form and methods

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit="onSubmit">
+    <label for="new-todo-input">What should be done?</label>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model="label"
+    />
+    <button type="submit">Add new task</button>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      console.log("labelin arvo:", this.label);
+    },
+  },
+  data() {
+    return { label: "" };
+  },
+};
+</script>
+
+<style scoped></style>
